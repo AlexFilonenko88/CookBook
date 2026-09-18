8ede4758ee83ba87ea0728508b21c881fb801cf7
AlexFilonenko88 <af88_88@mail.ru>
2026-09-18 15:59:58 +0300

    Add method RecipeUpdate

--- cookbook/schemas/recipe.py
+++ cookbook/schemas/recipe.py
@@ -13,5 +13,9 @@ class RecipeCreate(RecipeBase):
     ingredient: list[IngredientCreate]
 
 
-class RecipeUpdate(RecipeBase):
+class RecipeUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
     ingredient: list[IngredientRead] = Field(default_factory=list)
+
+    model_config = ConfigDict(from_attributes=True)
